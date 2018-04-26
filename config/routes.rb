c60766236b4f7ce9c1774e8c631936caba7f9879
ghildreth <gabrielantonhildreth@gmail.com>
2018-04-26 04:07:36 +0000

    added bcrypt functionality with secure hash for logging in and out AND regisration

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,13 @@ Rails.application.routes.draw do
     resources :categories, only: [:show, :index, :create, :new]
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -72,4 +79,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
