3ebc57037240c3f4601afec1994d08bc16a4b5d3
ghildreth <gabrielantonhildreth@gmail.com>
2018-04-27 22:07:13 +0000

    added the correct path for the form submission, now off to build the Review controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+  belongs_to :user
 
   validates :product_id, :user_id, :description, presence: true
   validates :rating, :inclusion => 1..5
