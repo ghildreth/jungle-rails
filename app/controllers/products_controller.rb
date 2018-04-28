c99f23b6fb399a8cab34c8ef57eff000f3e18a92
ghildreth <gabrielantonhildreth@gmail.com>
2018-04-28 00:22:07 +0000

    hide the review form for non logged in users and also added before_filter in the Review controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,13 @@
 class ProductsController < ApplicationController
 
-  def index
-    @products = Product.all.order(created_at: :desc)
-  end
 
   def show
     @product = Product.find params[:id]
   end
+
+  def index
+    @products = Product.all.order(created_at: :desc)
+  end
+
 
 end
